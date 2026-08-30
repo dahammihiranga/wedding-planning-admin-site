96865b21f7d43e1df886bf3e213f0de944bc07cf
Daham <daham@creatit.com.au>
2026-08-31 00:21:34 +0530

    invoice pdf ui fix

--- app/page.js
+++ app/page.js
@@ -6889,7 +6889,7 @@ Chathu Wedding Planners
                   className="absolute z-30 text-center"
                   style={{
                     left: "245px",
-                    top: "1040px",
+                    top: "1034px",
                     width: "310px",
                     height: "22px",
                     fontSize: "12px",
@@ -6906,7 +6906,7 @@ Chathu Wedding Planners
                   className="absolute z-30 text-center"
                   style={{
                     left: "245px",
-                    top: "1101px",
+                    top: "1099px",
                     width: "310px",
                     height: "22px",
                     fontSize: "12px",
